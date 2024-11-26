524aa7030046c31df1299d0b038bc01786c63fa5
grigoriirub <155987994+grigoriirub@users.noreply.github.com>
2024-11-26 16:59:49 +0200

    Create rectanglecalc.py

---
+++ day3/rectanglecalc.py
@@ -0,0 +1,6 @@
+length = input("Length = ")
+width = input("Width = ")
+area = int(length) * int(width)
+circumference = 2*int(width) + 2*int(length)
+print(f"Area of the rectangle: {area}")
+print(f"Circumference of the rectangle: {circumference}")
